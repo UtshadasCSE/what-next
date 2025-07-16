4ee32c61e5a90604ccaa2b70c63273b043637d38
UtshadasCSE <utshachandradas@gmail.com>
2025-07-16 09:54:41 +0600

    Mastering NextJs With Stacky Patel

--- src/app/products/[productId]/page.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const Page = async ({ params }: { params: { productId: string } }) => {
-  const productId = await params.productId;
-  return <div>Product ID for :{productId}</div>;
-};
-
-export default Page;
